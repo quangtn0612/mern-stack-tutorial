982e6523c4c61e55049e53d9cdc16864924812ce
Quang Nguyen <06121997ax@gmail.com>
2021-06-30 01:38:31 +1000

    first build backend

---
+++ backend/models/exercise.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+    username: {type: String, require: true},    
+    description: {type: String, require: true}, 
+    duration: {type: Number, require: true},
+    data: {type: Date, require: true}
+},{
+    timestamps: true
+});
+
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+module.exports = Exercise;
